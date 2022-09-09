c5ecf87b06a30a66d16509f4f7cdeb50655c7f98
NessaAz <vanessa.azenwa@gmail.com>
2022-09-09 22:18:24 +0300

    add views, urls and templates for chat room

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,3 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 
-# Create your views here.
+rooms =[
+    {'id':1, 'name': 'intro to data structures'},
+    {'id':2, 'name': 'intro to data algorithms'},
+    {'id':3, 'name': 'full stack development'},
+
+]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'home.html', context)
+
+def room(request):
+    return render(request, 'room.html')
+
