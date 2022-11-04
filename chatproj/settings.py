fe5d068bf86cf20b4bf966e9807d0d41f65d20d6
NessaAz <vanessa.azenwa@gmail.com>
2022-11-04 22:29:02 +0300

    freeze dependencies and adjust timezone

--- chatproj/settings.py
+++ chatproj/settings.py
@@ -1,4 +1,3 @@
-from distutils.command.config import config
 import os
 from pathlib import Path
 from decouple import config
@@ -99,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
